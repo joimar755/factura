7e94b9de56a8f55538159f9e0226f345062faa36
Edson Galan <edd.galan@hotmail.com>
2020-12-04 21:15:44 -0600

    Depurando código

--- index.php
+++ index.php
@@ -4,28 +4,25 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> Inserta algún título </title>
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <link href="css/bootstrap-theme.css" rel="stylesheet">
   <link href="css/jquery.dataTables.min.css" rel="stylesheet">
   <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
   <script type="text/javascript" src="js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/jquery.dataTables.min.js"></script>
- 
-
 
   <script>
     $(document).ready(function() {
+      /* ..:: DataTable ::.. */
       $('#mitabla').DataTable({
-
+        "language": {
+          "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"  //Configuración a Español
+        }
       });
 
-    });
-  </script>
-
-  <script>
-    $(document).ready(function() {
+      /*..:: BTN Enviar ::.. */
       $("#enviar").click(function() {
-
         var libras = $("#libra").val();
         var gramos = $("#gramos").val();
 
@@ -35,174 +32,106 @@
         }, function(data) {
           $("#resultado").html(data);
         });
-
-
       });
 
     });
   </script>
-
-
-
 </head>
-
-
-<body>
- 
-
-
-
-
-
-
+<body style="padding-left:30px; padding-right:30px;">
   <h1> listado de productos</h1>
-
   <div>
     <a href="factura/cotizar.php"><button class="btn btn-primary">COTIZACION</button></a>
     <a href="reporte.php"><button class="btn btn-primary">INFORME VENTA</button></a>
     <a href="pdf1.php"><button class="btn btn-primary">DETALLE VENTA</button></a>
   </div>
 
-  <div>
+  <div style="margin-top:15px;">
+    <div class="col">
+      <label> precio libra</label> <input type="text" name="libra" id="libra" required> </br>
+    </div>
+    <div class="col" style="margin-top:5px;">
+      <label>gramos</label> <input type="text" name="gramos" id="gramos" required> </br>
+    </div>
+    <div class="col" style="margin-top:5px";>
+      <input type="submit" name="calcular" value="calcular" id="enviar" class="btn btn-success">
+    </div>
+    <div id="resultado">
 
-    <br>
-    <label> precio libra</label> <input type="text" name="libra" id="libra" required> </br>
-    <br>
-    <label>gramos</label> <input type="text" name="gramos" id="gramos" required> </br>
-
-
-
-    <input type="submit" name="calcular" value="calcular" id="enviar">
-
-    <div id="resultado"></div>
-
-
-
+    </div>
   </div>
 
 
+  <table border="1" class="table table-striped" id="mitabla"></br>
+    <thead>
+      <th>ID</th>
+      <th>nombre</th>
+      <th>precio</th>
+      <th>stock</th>
+      <th>añadir a venta</th>
+    </thead>
+    <tbody>
+    <?php
+      require_once "model/data.php";
+      error_reporting(0);
+      $d = new Data();
+      $productos = $d->getProductos();
+      $cont = 0;
+      foreach ($productos as $p) {
+        echo "<tr>";
+        echo "<td>.$p->id.</td>";
+        echo "<td>.$p->nombre.</td>";
+        echo "<td>.$p->precio.</td>";
+        echo "<td>.$p->stock.</td>";
+        echo "<td>";
+        //echo "<form action='controller/agregar.php' method='post' id='formulario'>";
+        echo "<form   method='POST' id='formulario'>";
+        echo "<input type='hidden' name='txtID" . $cont . "'      id='id" . $cont . "' value='" . $p->id . "'>";
+        echo "<input type='hidden' name='txtnombre" . $cont . "'   id='nombre" . $cont . "'   value='" . $p->nombre . "'>";
+        echo "<input type='hidden' name='txtprecio" . $cont . "'   id='precio" . $cont . "'  value='" . $p->precio . "'>";
+        echo "<input type='hidden' name='txtstock" . $cont . "'       id='stock" . $cont . "'  value='" . $p->stock . "'>";
+        echo "<input type='number' name='txtcantidad" . $cont . "'     id='cantidad" . $cont . "'   step='0.1' min='0' >";
+        echo "<button  type='button' id='btn_generar" . $cont . "'   onclick='agregar(" . $cont . ")'   class='btn btn-primary'> Añadir </button>";
+        echo "<a href=''><input type='button' name='btneditar' id='' value='editar' ></a>";
+        echo "</form>";
+        echo "</td>";
+        echo "</tr>";
+     }
+     $cont++;
+    ?>
+    </tbody>
+  </table>
 
-</body>
-
-
-
-
-<table border="1" class="table table-striped" id="mitabla"></br>
-
-
-  <thead>
-    <th>ID</th>
-    <th>nombre</th>
-    <th>precio</th>
-    <th>stock</th>
-    <th>añadir a venta</th>
-
-
-
-  </thead>
-
-
+  <!-- ..:: Listado de Ventas ::.. -->
+  <a href="vista/ventas.php">listado de ventas</a>
   <?php
+    $msg_agotado1 = "<script> alert('el producto esta agotado'); window.location= 'index.php'; </script>";
+    $msg_agotado2 = "<script> alert('el producto esta agotado'); window.location= 'index.php'; </script>";
 
-  require_once "model/data.php";
-
-
-
-
-  error_reporting(0);
-
-  $d = new Data();
-
-
-  $productos = $d->getProductos();
-
-  $cont = 0;
-  foreach ($productos as $p) {
-    echo "<tr>";
-    echo "<td>.$p->id.</td>";
-    echo "<td>.$p->nombre.</td>";
-    echo "<td>.$p->precio.</td>";
-    echo "<td>.$p->stock.</td>";
-
-
-    echo "<td>";
-    //echo "<form action='controller/agregar.php' method='post' id='formulario'>";
-    echo "<form   method='POST' id='formulario'>";
-          echo "<input type='hidden' name='txtID" . $cont . "'      id='id" . $cont . "' value='" . $p->id . "'>";
-          echo "<input type='hidden' name='txtnombre" . $cont . "'   id='nombre" . $cont . "'   value='" . $p->nombre . "'>";
-          echo "<input type='hidden' name='txtprecio" . $cont . "'   id='precio" . $cont . "'  value='" . $p->precio . "'>";
-          echo "<input type='hidden' name='txtstock" . $cont . "'       id='stock" . $cont . "'  value='" . $p->stock . "'>";
-          echo "<input type='number' name='txtcantidad" . $cont . "'     id='cantidad" . $cont . "'   step='0.1' min='0' >";
-          echo "<button  type='button' id='btn_generar" . $cont . "'   onclick='agregar(" . $cont . ")'   class='btn btn-primary'> Añadir </button>";
-          echo "<a href=''><input type='button' name='btneditar' id='' value='editar' ></a>";
-    echo "</form>";
-
-
-    echo "</td>";
-    echo "</tr>";
-  }
-  $cont++;
+    if (isset($_GET["m"])) {
+      $m = $_GET["m"];
+      switch ($m) {
+        case "1": echo $msg_agotado1;
+                  break;
+        case "2": echo $msg_agotado2
+                  break;
+      }
+      
+    }
 
   ?>
 
+  <h1>listado de compra</h1>
+  <div>
+    <table class="table table-dark">
 
+      <tbody id="cuerpoTabla">
 
+      </tbody>
 
-</table>
+    </table>
 
-
-
-<a href="vista/ventas.php">listado de ventas</a>
-<?php
-if (isset($_GET["m"])) {
-  $m = $_GET["m"];
-  switch ($m) {
-    case "1":
-      echo "<script>
-                       alert('el producto esta agotado');
-                       window.location= 'index.php'
-           </script>";
-      break;
-    case "2":
-      echo "<script>
-                         alert('el producto esta agotado');
-                         window.location= 'index.php'
-             </script>";
-      break;
-  }
-}
-
-
-
-
-
-?>
-
-
-
-<h1>listado de compra</h1>
-<div>
-  <table class="table table-dark">
-
-    <tbody id="cuerpoTabla">
-
-    </tbody>
-
-  </table>
-
-</div>
-
-
-
-
-
-
-
-
-<script src="agregar.js"></script>
-
-
-
+  </div>
 </body>
+<script src="agregar.js"></script>
 
 </html>
--- model/data.php
+++ model/data.php
@@ -6,153 +6,110 @@ require_once "Detalle.php";
 
 
  class Data{
-    private $con; 
-
-public function __construct(){
-
-  $this->con = new Conexion("localhost", "ventas", "root", "");
- 
- 
-}
-
-   public function getProductos(){
-
-     $productos = array();
-
-     $query = "select * from Producto ";
-     $res = $this->con->ejecutar($query);
-      while ($reg = mysql_fetch_array($res)) {
-
-      $p = new Producto();
-
-      $p->id = $reg[0];
-      $p->nombre = $reg[1];
-      $p->precio = $reg[2];
-      $p->stock = $reg[3];
- 
-
-  array_push($productos, $p);
-
-
-}
-
-     return $productos;
-
-   }
- 
-   public function getventas(){
-
-     $ventas = array();
-
-     $query = "select * from venta";
-     $res = $this->con->ejecutar($query);
-     while ($reg = mysql_fetch_array($res)) {
-
-    $v = new venta();
- 
-    $v->id = $reg[0];
-    $v->fecha = $reg[1];
-    $v->total = $reg[2];
-
-
-
-  array_push($ventas, $v);
-
-
-  }
-
-     return $ventas;
-
-   }
-
-
-   public function getdetalles($idventa){
-    $query = "select d.id,p.nombre,d.cantidad, d.subtotal, p.precio
-    from detalle d, producto p
-    where d.producto = p.id and
-    d.venta = '$idventa'";
-
-    $detalles = array();
-
-    $res = $this->con->ejecutar($query);
-
-    while ($reg = mysql_fetch_array($res)){
-       $d = new Detalle();
-       
-       $d->id = $reg[0];
-       $d->nombreproducto = $reg[1];
-       $d->cantidad = $reg[2];
-       $d->subtotal = $reg[3];
-       $d->precio = $reg[4];
-
-
-       array_push($detalles, $d);
+    private $con;
+    public function __construct(){
+      $this->con = new Conexion("localhost", "ventas", "root", "");
     }
 
+    /* ..:: Función que regresa los productos ::.. */
+    public function getProductos(){
+      $productos = array();
 
+      $query = "select * from Producto ";
+      $res = $this->con->ejecutar($query);
+
+      while ($reg = mysql_fetch_array($res)) {
+        $p = new Producto();
+
+        $p->id = $reg[0];
+        $p->nombre = $reg[1];
+        $p->precio = $reg[2];
+        $p->stock = $reg[3];
+
+        array_push($productos, $p);
+      }
+
+      return $productos;
+    }
+
+    /* ..:: Función que regresa las ventas ::.. */
+    public function getventas(){
+      $ventas = array();
+      $query = "select * from venta";
+      $res = $this->con->ejecutar($query);
+
+      while ($reg = mysql_fetch_array($res)) {
+        $v = new venta();
+        $v->id = $reg[0];
+        $v->fecha = $reg[1];
+        $v->total = $reg[2];
+        array_push($ventas, $v);
+      }
+      return $ventas;
+    }
+
+    /* ..:: Función que regresa los detalles ::.. */
+    public function getdetalles($idventa){
+      $query = "select d.id,p.nombre,d.cantidad, d.subtotal, p.precio
+      from detalle d, producto p
+      where d.producto = p.id and
+      d.venta = '$idventa'";
+
+      $detalles = array();
+
+      $res = $this->con->ejecutar($query);
+
+      while ($reg = mysql_fetch_array($res)){
+        $d = new Detalle();
+        $d->id = $reg[0];
+        $d->nombreproducto = $reg[1];
+        $d->cantidad = $reg[2];
+        $d->subtotal = $reg[3];
+        $d->precio = $reg[4];
+
+        array_push($detalles, $d);
+      }
       return $detalles;
+    }
 
-  }
-
-
-
-
+    /* ..:: Función que crea una venta ::.. */
    public function crearventa($listaproductos,  $total){
-
      $query = "insert into venta values(null, now(), $total)";
      $this->con->ejecutar($query);
- 
-      $idultimaventa = 0;
-
+     $idultimaventa = 0;
      $query = "select max(id) from venta";
      $res = $this->con->ejecutar($query);
+
      if ($reg = mysql_fetch_array($res)) {
 
-    } 
-          $idultimaventa = $reg[0];
+     }
+
+     $idultimaventa = $reg[0];
 
      // insert del detalle
-          foreach ($listaproductos as $p) {
-            $query = "insert into detalle values(null,
-              '".$idultimaventa."',
-              '".$p->id."',
-              '".$p->cantidad."',
-              '".$p->subtotal."')";
+     foreach ($listaproductos as $p) {
+       $query = "insert into detalle values(null,
+       '".$idultimaventa."',
+       '".$p->id."',
+       '".$p->cantidad."',
+       '".$p->subtotal."')";
+       $this->con->ejecutar($query);
+       $this->actualizarstock($p->id,$p->cantidad);
+     }
+   }
 
-             $this->con->ejecutar($query);
-             
-             $this->actualizarstock($p->id,$p->cantidad);
-            
-       }
-
-      
- 
- 
-
-       }
-
-     public function actualizarstock($id , $stockAdescontar){ 
-      $query = "select stock from producto where id = $id"; 
-      $res = $this->con->ejecutar($query);
-      $stockActual = 0;
-      if ($reg = mysql_fetch_array($res)) {
-        $stockActual = $reg[0];
-  }
-
-
-
-      $stockActual -= $stockAdescontar;  
-
-      $query="update producto set stock = $stockActual where id = $id"; 
-      $this->con->ejecutar($query);
-
-     } 
-     
-     while
-     
-
-
-
+   /* ..:: Función que actualiza el stock ::.. */
+   public function actualizarstock($id , $stockAdescontar){
+     $query = "select stock from producto where id = $id";
+     $res = $this->con->ejecutar($query);
+     $stockActual = 0;
+     if ($reg = mysql_fetch_array($res)) {
+       $stockActual = $reg[0];
+     }
+     $stockActual -= $stockAdescontar;
+     $query="update producto set stock = $stockActual where id = $id";
+     $this->con->ejecutar($query);
+   }
   }
 
 
@@ -174,7 +131,6 @@ public function __construct(){
           }
 
  }*/
-
 
 
  ?>
